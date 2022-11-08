0f6b89866ff4e50953db7a6ac97cf450946c2bdf
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-11-08 15:49:54 +0300

    first microservice completed

---
+++ Demo/eshop/Services/Catalog/eshop.Catalog.API/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using eshop.Catalog.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eshop.Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var result = _productService.GetProducts();
+            return Ok(result);
+        }
+    }
+}
