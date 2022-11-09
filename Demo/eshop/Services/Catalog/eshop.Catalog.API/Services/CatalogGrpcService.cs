ceabc718f62422ec2ac18cb7ac9003eede42053f
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-11-09 10:37:36 +0300

    Grpc added to catalog microservice

---
+++ Demo/eshop/Services/Catalog/eshop.Catalog.API/Services/CatalogGrpcService.cs
@@ -0,0 +1,23 @@
+﻿using eshop.Catalog.API.Proto;
+using eshop.Catalog.Application;
+using Grpc.Core;
+
+namespace eshop.Catalog.API.Services
+{
+    public class CatalogGrpcService : CatalogService.CatalogServiceBase
+    {
+        private readonly IProductService productService;
+
+        public CatalogGrpcService(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public override async Task<ProductMessage> GetProducts(EmptyParameter request, ServerCallContext context)
+        {
+
+            var product = productService.GetProducts().FirstOrDefault();
+            return new ProductMessage { Name = product.Name, Price = 5 };
+        }
+    }
+}
